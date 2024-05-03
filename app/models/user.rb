41318fe6966eed4c116470e1f079ede0d18fb4db
DHerrera91 <diegoherreraruano91@gmail.com>
2024-05-03 12:58:10 -0600

    Got the endpoints for user authentication working

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,8 +4,4 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :validatable, :jwt_authenticatable, jwt_revocation_strategy: self
-    validates :first_name, presence: true, length: {minimum: 2}
-    validates :last_name, presence: true, length: {minimum: 2} 
-    validates :password, presence: true, length: {minimum: 8}
-    validates :email, presence: true, uniqueness: true
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  get 'current_user', to: 'current_user#index'
   # Existing routes for Devise
   devise_for :users, path: '', path_names: {
     sign_in: 'login',
